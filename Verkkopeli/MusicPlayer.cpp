0ff7d22dd66c99f759f9b35a6a26988da45d143b
SikSakJiiPee <juissi93@hotmail.com>
2018-04-13 13:45:46 +0300

    Uudet musiikit

--- Verkkopeli/MusicPlayer.cpp
+++ Verkkopeli/MusicPlayer.cpp
@@ -6,8 +6,8 @@ MusicPlayer::MusicPlayer()
 , mFilenames()
 , mVolume(100.f)
 {
-	mFilenames[Music::MenuTheme]    = "Media/Music/MenuTheme.ogg";
-	mFilenames[Music::MissionTheme] = "Media/Music/MissionTheme.ogg";
+	mFilenames[Music::MenuTheme]    = "Media/Music/hungerland.wav";
+	mFilenames[Music::MissionTheme] = "Media/Music/impact.wav";
 }
 
 void MusicPlayer::play(Music::ID theme)
